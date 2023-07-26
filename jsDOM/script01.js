13335969de2d7efcfec5d066a0ca9a8a435965b5
anhelikscbr1 <anhelik97@gmail.com>
2023-07-25 20:59:33 -0600

    js events

---
+++ jsDOM/script01.js
@@ -0,0 +1,15 @@
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function(e){
+    console.log('click');
+    console.log(e);
+});
+
+//btn.onclick = function(){
+    //console.log("second click");
+//};
+
+function clickk(){
+    console.log('click from html');
+}
